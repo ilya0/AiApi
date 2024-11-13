18933fa107f018f0cf464062c9d67dda79420801
ilya0 <iosovets@gmail.com>
2024-11-13 15:57:07 -0800

    added post and put

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+items = []
 
+##get route to create items
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {" Welcome to AiAPi"}
 
+##post route create items
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def get_item(item_id: int) -> str:
     if item_id < len(items):
-        return {"item_id": item_id, "q": q}
+        return items[item_id]
     else:
         raise HTTPException(status_code=404, detail="Item not found")
 
